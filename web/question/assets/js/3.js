acd3130dbd08a0a4eb0de1e714cc2b5a35a26b10
ـــيث 𝕃𝕒𝕚𝕥𝕙 لـــ <127667772+laitho8@users.noreply.github.com>
2024-08-06 14:40:44 +0300

    Add files via upload

---
+++ web/question/assets/js/3.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = function(e, s) {
+    return {
+        __rewrites: {
+            beforeFiles: [],
+            afterFiles: [],
+            fallback: []
+        },
+        "/": [e, s, "static/chunks/pages/index-508c0ff99cdb2377.js"],
+        "/_error": ["static/chunks/pages/_error-54de1933a164a1ff.js"],
+        "/review_guidelines": [e, s, "static/chunks/pages/review_guidelines-44389679f780eae8.js"],
+        sortedPages: ["/", "/_app", "/_error", "/review_guidelines"]
+    }
+}("static/chunks/921-9bdce993aabe65a7.js", "static/css/b66ab0c7ede20f5c.css"),
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
---
+++ web/question/assets/js/6.js
@@ -0,0 +1,353 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[405], {
+    8312: function(e, t, s) {
+        (window.__NEXT_P = window.__NEXT_P || []).push(["/", function() {
+            return s(5423)
+        }
+        ])
+    },
+    5423: function(e, t, s) {
+        "use strict";
+        s.r(t),
+        s.d(t, {
+            default: function() {
+                return d
+            }
+        });
+        var n = s(5893)
+          , a = s(6465)
+          , i = s.n(a)
+          , o = s(9008)
+          , c = s.n(o)
+          , l = s(9854)
+          , r = s.n(l)
+          , m = s(7294);
+        let u = ()=>(0,
+        n.jsxs)("svg", {
+            xmlns: "http://www.w3.org/2000/svg",
+            viewBox: "0 0 24 24",
+            width: "24",
+            height: "24",
+            children: [(0,
+            n.jsx)("path", {
+                fill: "none",
+                d: "M0 0h24v24H0z"
+            }), (0,
+            n.jsx)("path", {
+                d: "M19 11h-6V5c0-.55-.45-1-1-1s-1 .45-1 1v6H5c-.55 0-1 .45-1 1s.45 1 1 1h6v6c0 .55.45 1 1 1s1-.45 1-1v-6h6c.55 0 1-.45 1-1s-.45-1-1-1z",
+                fill: "#777"
+            })]
+        });
+        var _ = [{
+            title: "حول كونايو",
+            question: "ما هو كونايو ؟",
+            answer: "تطبيق عربي للتتبع و التواصل حول الأنمي و المانجا، ستجد كل جديد يتعلق بالأنمي و المانجا، تصفح بسرعة معلومات الأعمال الموجودة، قم بقراءة مراجعات و توصيات الأعضاء الآخرين، ابق على اطلاع دائم على مواسم عرض الأعمال السابقة منها و اللاحقة، قم بتحديث تتبعك للحلقات و الفصول بسرعة كبيرة , شارك رأيك و مراجعاتك الخاصة مع الجميع , توصل بآخر أخبار الأنمي و المانجا، بالإضافة إلى المزيد من المميزات."
+        }, {
+            title: "حول كونايو",
+            question: "هل كونايو مجاني ؟",
+            answer: "نعم مجاني ومتاح لأنظمة الios والAndroid"
+        }, {
+            title: "حول كونايو",
+            question: "هل يمكن مشاهدة الانميات من خلال كونايو ؟",
+            answer: "كلا ، لا يمكن"
+        }, {
+            title: "حول كونايو",
+            question: "هل يمكن قراءة المانجا من خلال كونايو ؟",
+            answer: "كلا ، لا يمكن"
+        }, {
+            title: "النقاط",
+            question: "ما هي النقاط ؟",
+            answer: "هي نقاط رقمية في كونايو"
+        }, {
+            title: "النقاط",
+            question: "ما فائدة النقاط ؟",
+            answer: "تستطيع استخدام النقاط لتحويلها الى نجوم"
+        }, {
+            title: "النقاط",
+            question: "كيف احصل على النقاط ؟",
+            answer: ".ا تستطيع كسب النقاط عن طريق تعديل انمي او مانجا او تعديل الشخصيات او التعليق في الحلقات ، داخل التطبيق بعد الموافقة عليها من المشرفين كذلك من خلال المشاركة في الفعاليات داخل التطبيق"
+        }, {
+            title: "النجوم",
+            question: "ما هي النجوم ؟",
+            answer: "عملة حصرية خاصة ل تطبيق كونايو تستخدام داخل التطبيق فقط"
+        }, {
+            title: "النجوم",
+            question: "كيف احصل على النجوم ؟",
+            answer: "تستطيع كسب النجوم عن طريق تحويل النقاط الى نجوم او استلامها من الاخرين"
+        }, {
+            title: "النجوم",
+            question: "كيف ارسل النجوم للاخرين ؟",
+            answer: "بعد ان يتم تفعيل الخيار لك ، يظهر خيار ارسال نجوم عند الدخول الى تفاصيل المنشور"
+        }, {
+            title: "النجوم",
+            question: "متى يتم تفعيل ارسال النجوم لحسابي ؟",
+            answer: "يتم تفعيل هذا الخيار بعد مراجعة حسابك ."
+        }, {
+            title: "النجوم",
+            question: "ما هي فرصة الحصول على خيار ارسال النجوم ؟",
+            answer: "لزيادة فرصتك للحصول عليها التركيز على محتوى الانمي  و المانجا و عدم مخالفة القوانين كذلك عدم طلب الخيار من قبل المشرفين "
+        }, {
+            title: "المساهمة",
+            question: "ما هي المساهمة ؟",
+            answer: "المساهمة من خلال تعديل بيانات الانميات او المانجا او الشخصية و الموافقة عليها من قبل المشرفين"
+        }, {
+            title: "المساهمة",
+            question: "كيف يتم المساهمة ؟",
+            answer: "يمكنك المساهمة من خلال الذهاب الى تفاصيل الانميات او المانجا او الشخصية ، سوف يظهر لديك خيار التعديل هناك"
+        }, {
+            title: "المساهمة",
+            question: "لماذا لا يظهر لدي خيار المساهمة؟",
+            answer: "يجب أن يكون حسابك قد مضى عليه أسبوع على الأقل، وألا يكون نظام التشغيل لديك أندرويد 4.4 أو أقل. في بعض الأحيان، قد يتم حظرك من قبل المشرفين في حالة مخالفتك للقواعد أكثر من مرة."
+        }, {
+            title: "المساهمة",
+            question: "متى يتم الموافقة على المساهمة؟",
+            answer: "الموافقة على المساهمة تتم بعد مراجعة المشرفين لها والموافقة عليها."
+        }, {
+            title: "+ كونايو",
+            question: "ما هو كونايو + ؟",
+            answer: "كونايو + هو اشتراك شهري باستخدام النجوم فقط يتيح لك ميزات لحسابك"
+        }, {
+            title: "+ كونايو",
+            question: "ما هو الاشتراك التلقائي ؟",
+            answer: "يتم تجديد الاشتراك التلقائي كل شهر لكونايو + اذا لم تقم بايقاف التجديد من خلال صفحة الاشتراك"
+        }, {
+            title: "الحساب",
+            question: "أنشأت حسابًا جديدًا ولكنني لا أستطيع نشر الصور أو الفيديوهات؟",
+            answer: "عادة ما يتطلب الحسابات الجديدة بعض الوقت قبل أن تتمكن من نشر الصور أو الفيديوهات."
+        }, {
+            title: "الحساب",
+            question: "لقد حذفت الحساب بالخطأ، هل يمكن استعادته؟",
+            answer: "للأسف، عادة ما يكون استعادة الحساب بعد حذفه مستحيلًا."
+        }, {
+            title: "الحساب",
+            question: "هل يوجد حد لمتابعة الاخرين ؟",
+            answer: "نعم الحد الاقصى للمتابعة هو 5000 "
+        }];
+        let x = ()=>{
+            let[e,t] = (0,
+            m.useState)(null)
+              , s = (0,
+            m.useRef)({})
+              , a = s=>{
+                t(e === s ? null : s)
+            }
+            ;
+            (0,
+            m.useEffect)(()=>{
+                let e = new URLSearchParams(window.location.search)
+                  , s = e.get("q");
+                s && t(s)
+            }
+            , []),
+            (0,
+            m.useEffect)(()=>{
+                let t = new URLSearchParams;
+                if (null !== e ? t.set("q", e) : t.delete("q"),
+                window.history.replaceState(null, "", "?".concat(t.toString())),
+                null !== e && s.current[e]) {
+                    var n;
+                    null === (n = s.current[e]) || void 0 === n || n.scrollIntoView({
+                        behavior: "smooth"
+                    })
+                }
+            }
+            , [e]);
+            let i = _.reduce((e,t)=>(e[t.title] ? e[t.title].push(t) : e[t.title] = [t],
+            e), {});
+            return (0,
+            n.jsxs)("div", {
+                className: "".concat(r().container, " dark-theme ").concat(r().lightTheme),
+                children: [(0,
+                n.jsx)("h2", {
+                    className: r().pageTitle,
+                    children: "الأسئلة الشائعة"
+                }), Object.entries(i).map((t,i)=>{
+                    let[o,c] = t;
+                    return (0,
+                    n.jsxs)("div", {
+                        children: [(0,
+                        n.jsx)("h4", {
+                            className: r().sectionTitle,
+                            children: o
+                        }), c.map((t,o)=>{
+                            let c = "s".concat(i, "q").concat(o);
+                            return (0,
+                            n.jsxs)("div", {
+                                className: r().faq,
+                                ref: e=>s.current[c] = e,
+                                children: [(0,
+                                n.jsxs)("h3", {
+                                    onClick: ()=>a(c),
+                                    className: r().question,
+                                    children: [t.question, (0,
+                                    n.jsx)("span", {
+                                        className: e === c ? r().iconExpanded : r().icon,
+                                        children: (0,
+                                        n.jsx)(u, {})
+                                    })]
+                                }), (0,
+                                n.jsx)("p", {
+                                    className: e === c ? r().answerExpanded : r().answer,
+                                    children: t.answer
+                                })]
+                            }, c)
+                        }
+                        )]
+                    }, o)
+                }
+                )]
+            })
+        }
+        ;
+        function d() {
+            return (0,
+            n.jsxs)(n.Fragment, {
+                children: [(0,
+                n.jsxs)(c(), {
+                    children: [(0,
+                    n.jsx)("title", {
+                        className: "jsx-c585a93b9e043274",
+                        children: "الأسئلة الشائعة"
+                    }), (0,
+                    n.jsx)("meta", {
+                        name: "description",
+                        content: "كونايو | الأسئلة الشائعة",
+                        className: "jsx-c585a93b9e043274"
+                    }), (0,
+                    n.jsx)("meta", {
+                        name: "viewport",
+                        content: "width=device-width, initial-scale=1",
+                        className: "jsx-c585a93b9e043274"
+                    }), (0,
+                    n.jsx)("link", {
+                        rel: "icon",
+                        href: "/favicon.ico",
+                        className: "jsx-c585a93b9e043274"
+                    }), (0,
+                    n.jsx)("meta", {
+                        property: "og:image",
+                        content: "https://kunaiu.com/img/kunaiu_share_fb.png",
+                        className: "jsx-c585a93b9e043274"
+                    }), (0,
+                    n.jsx)("meta", {
+                        property: "og:type",
+                        content: "website",
+                        className: "jsx-c585a93b9e043274"
+                    }), (0,
+                    n.jsx)("meta", {
+                        name: "twitter:card",
+                        content: "summary",
+                        className: "jsx-c585a93b9e043274"
+                    }), (0,
+                    n.jsx)("meta", {
+                        name: "twitter:site",
+                        content: "@Kunaiu",
+                        className: "jsx-c585a93b9e043274"
+                    }), (0,
+                    n.jsx)("meta", {
+                        name: "twitter:image",
+                        content: "https://kunaiu.com/img/kunaiu_share.tw.png",
+                        className: "jsx-c585a93b9e043274"
+                    }), (0,
+                    n.jsx)("link", {
+                        rel: "apple-touch-icon",
+                        href: "https://kunaiu.com/img/favicon.png",
+                        className: "jsx-c585a93b9e043274"
+                    }), (0,
+                    n.jsx)("link", {
+                        rel: "icon",
+                        type: "image/png",
+                        href: "https://kunaiu.com/img/favicon.png",
+                        className: "jsx-c585a93b9e043274"
+                    })]
+                }), (0,
+                n.jsx)(i(), {
+                    id: "c585a93b9e043274",
+                    children: ".header.jsx-c585a93b9e043274{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-pack:start;-webkit-justify-content:flex-start;-moz-box-pack:start;-ms-flex-pack:start;justify-content:flex-start;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center;padding-top:20px;padding-left:15px}.logo-container.jsx-c585a93b9e043274{margin-right:auto}"
+                }), (0,
+                n.jsx)("header", {
+                    className: "jsx-c585a93b9e043274 header",
+                    children: (0,
+                    n.jsx)("div", {
+                        className: "jsx-c585a93b9e043274 logo-container",
+                        children: (0,
+                        n.jsx)("a", {
+                            href: "https://kunaiu.com",
+                            className: "jsx-c585a93b9e043274",
+                            children: (0,
+                            n.jsx)("img", {
+                                src: "https://kunaiu.com/img/logo_kunaiu.165x31.png",
+                                alt: "Logo",
+                                style: {
+                                    width: "160px"
+                                },
+                                className: "jsx-c585a93b9e043274 logo"
+                            })
+                        })
+                    })
+                }), (0,
+                n.jsx)("main", {
+                    className: "jsx-c585a93b9e043274 " + (r().main || ""),
+                    children: (0,
+                    n.jsx)(x, {})
+                }), (0,
+                n.jsx)("footer", {
+                    style: {
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        paddingBottom: "20px"
+                    },
+                    className: "jsx-c585a93b9e043274",
+                    children: (0,
+                    n.jsx)("a", {
+                        href: "https://kunaiu.com",
+                        className: "jsx-c585a93b9e043274",
+                        children: (0,
+                        n.jsx)("img", {
+                            src: "https://kunaiu.com/img/logo_kunaiu.38x20.png",
+                            alt: "Logo",
+                            style: {
+                                width: "30px"
+                            },
+                            className: "jsx-c585a93b9e043274 logo"
+                        })
+                    })
+                })]
+            })
+        }
+    },
+    9854: function(e) {
+        e.exports = {
+            main: "Home_main__EtNt2",
+            description: "Home_description__Qwq1f",
+            code: "Home_code__aGV0U",
+            grid: "Home_grid__c_g6N",
+            card: "Home_card__7oz7W",
+            center: "Home_center__V0nxp",
+            logo: "Home_logo__80mSr",
+            thirteen: "Home_thirteen__ocdUI",
+            rotate: "Home_rotate__99GkW",
+            content: "Home_content___fOQz",
+            vercelLogo: "Home_vercelLogo__lhIxO",
+            pageTitle: "Home_pageTitle__aV_Go",
+            sectionTitle: "Home_sectionTitle__BMb1D",
+            answerExpanded: "Home_answerExpanded__tAdqp",
+            faq: "Home_faq__mwFYD",
+            question: "Home_question__lPGjk",
+            answer: "Home_answer__cTO8F",
+            icon: "Home_icon__ZLMsC",
+            iconExpanded: "Home_iconExpanded__SkvMA",
+            header: "Home_header__qwdnY",
+            subHeader: "Home_subHeader__whsrj",
+            list: "Home_list__9JfPL",
+            listItem: "Home_listItem__2LwsP",
+            textParagraph: "Home_textParagraph__l0m7N"
+        }
+    }
+}, function(e) {
+    e.O(0, [921, 774, 888, 179], function() {
+        return e(e.s = 8312)
+    }),
+    _N_E = e.O()
+}
+]);
